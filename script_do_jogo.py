2e59c2086fc92516c7121166d0cf058a67c2f3cb
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2024-01-30 17:16:59 -0300

    tratando alguns erros

--- script_do_jogo.py
+++ script_do_jogo.py
@@ -10,29 +10,36 @@ sleep(1)
 print("Gostaria de jogar Adivinhe o Número? ")
 sleep(1)
 print("0.Não \n1.Sim")
-escolha_de_jogar = int(input("Sua resposta: "))
+escolha_de_jogar = input("Sua resposta: ")
 
-
-if escolha_de_jogar == 0:
-    print(f"Ok, {nome_do_jogador}. Até logo! ")
+try:
+    if escolha_de_jogar == 0:
+        print(f"Ok, {nome_do_jogador}. Até logo! ")
     
-elif escolha_de_jogar == 1:
-    print(f"Vamos começar o jogo então {nome_do_jogador}!")
-    sleep(1)
-    print("Você tem 10 chances de acertar o número sorteado de 1 a 100")
-    sleep(2)
-    num_sorteado = random.randint(1,100)
-    quant_chutes = 0
-    while quant_chutes < 10:
-        num_chutado = int(input("Qual é o seu chute? "))
-        quant_chutes += 1
-        if num_chutado > num_sorteado:
-            print("Chute mais baixo!")
-        elif num_chutado < num_sorteado:
-            print("Chute mais alto!")
-        else:
-            print("Parabéns você acertou o número!")
-            break
-else:
-    print("Resposta inválida, digite 0 ou 1")
+    elif escolha_de_jogar == 1:
+        print(f"Vamos começar o jogo então {nome_do_jogador}!")
+        sleep(1)
+        print("Você tem 10 chances de acertar o número sorteado de 1 a 100")
+        sleep(2)
+        num_sorteado = random.randint(1,100)
+        quant_chutes = 0
+        while quant_chutes < 10:
+            num_chutado = int(input("Qual é o seu chute? "))
+            quant_chutes += 1
+            if num_chutado > num_sorteado:
+                print("Chute mais baixo!")
+            elif num_chutado < num_sorteado:
+                print("Chute mais alto!")
+            else:
+                print("Parabéns você acertou o número!")
+                break
+    elif type(escolha_de_jogar) != int:
+        raise TypeError
     
+    else:
+        raise ValueError
+    
+except TypeError:
+    print("Tipo inválido, sua resposta deve ser um número inteiro!")
+except ValueError:
+    print("Resposta inválida, digite 0 ou 1 por favor!")   
