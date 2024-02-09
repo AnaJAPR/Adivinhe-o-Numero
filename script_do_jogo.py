c7b154342b220106ecb3509378402898a0a0b4bf
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2024-02-09 17:47:36 -0300

    reestruturando a lógica do jogo

--- script_do_jogo.py
+++ script_do_jogo.py
@@ -2,25 +2,28 @@ import random
 from time import sleep
 
 # Desenvolver uma interface em que o usuário tenta adivinhar um número de 1 a 100 gerado aleatoriamente
-try:
-    nome_do_jogador = input("Olá, qual é o seu nome?  ")
-    sleep(1)
-    print(f"Bem-vindo(a) {nome_do_jogador}!")
-    sleep(1)
-    print("Gostaria de jogar Adivinhe o Número? ")
-    sleep(1)
-    print("0.Não \n1.Sim")
-    sleep(1)
-    escolha_de_jogar = int(input("Sua resposta: "))
 
+nome_do_jogador = input("Olá, qual é o seu nome?  ")
+sleep(2)
+print(f"Bem-vindo(a) {nome_do_jogador}!")
+sleep(1)
+print("Gostaria de jogar Adivinhe o Número? ")
+sleep(1)
+print("0.Não \n1.Sim")
+sleep(1)
+jogar = True
+while jogar == True:
     try:
+        escolha_de_jogar = int(input("Sua resposta: "))
         if escolha_de_jogar == 0:
             print(f"Ok, {nome_do_jogador}. Até logo! ")
-    
+            jogar = False
+            break
+
         elif escolha_de_jogar == 1:
-            print(f"Vamos começar o jogo então {nome_do_jogador}!")
+            print(f"Vamos jogar então {nome_do_jogador}!")
             sleep(1)
-            print("Você tem 10 chances de acertar o número sorteado de 1 a 100")
+            print("Você tem 10 chances de acertar o número sorteado de 1 a 100!")
             sleep(2)
             num_sorteado = random.randint(1,100)
             quant_chutes = 0
@@ -33,15 +36,29 @@ try:
                     print("Chute mais alto!")
                 else:
                     print("Parabéns, você acertou o número!")
-                    break
+                    quant_chutes = 10
+                    sleep(1)
+                    print("Gostaria de continuar o jogo?")
+                    sleep(1)
+                    print("0.Não \n1.Sim")
+                    sleep(1)
+                    jogar = True
         else:
             print("Resposta inválida, digite 0 ou 1")
-        
+            jogar = False
+            break
+         
     except:
-        print("Chute inválido, tente números inteiros de 0 a 100!")
+        print("Resposta inválida, vamos começar novamente...")
+        sleep(2)
+        print("Gostaria de jogar Adivinhe o Número? ")
+        sleep(1)
+        print("0.Não \n1.Sim")
+        sleep(1)
+        jogar = True
         
-except:
-    print("Resposta inválida, tente novamente!")
+
+    
     
     
     
